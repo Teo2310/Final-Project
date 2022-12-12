b0acae7282a072f08b66ff4be4586764cf6d5407
Teo2310 <110611615+Teo2310@users.noreply.github.com>
2022-12-12 19:16:21 +0400

    almost there

---
+++ script.js
@@ -0,0 +1,8 @@
+let burger=document.querySelector(".fa-bars");
+let mobileMenu=document.querySelector(".menubar");
+
+
+burger.addEventListener("click",function(){
+    mobileMenu.classList.toggle("active");
+
+})
